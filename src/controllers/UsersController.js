bf654607c4ab1915425a31ed124abf6c99acdd9a
luiz <afsilvaluiz@gmail.com>
2023-12-19 11:11:26 -0300

    Added to the utils folder for error control

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,9 +1,18 @@
+const AppError = require("../utils/AppError")
+
+const { default: test } = require("node:test")
+
 class UsersController {
   create(request, response) {
     const { name, email, password } = request.body
+    if (!name) {
+      throw new AppError("Name is a required field!")
+    }
 
-    response.json({ name, email, password })
+    response.status(201).json({ name, email, password })
   }
 }
 
 module.exports = UsersController
+
+
