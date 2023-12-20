75dff23cdb2632ce6d7082d0ba30ad38fe8a4524
luiz <afsilvaluiz@gmail.com>
2023-12-20 09:13:45 -0300

    Added the 'migrations' folder to automate table creation in SQLite

--- src/server.js
+++ src/server.js
@@ -1,21 +1,19 @@
 require("express-async-errors")
-
-
-const database = require("./database/sqlite/index")
+const migrationsRun = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError")
-const express = require("express")
 
+const express = require("express")
 const routes = require("./routes")
+
+migrationsRun()
 
 const app = express()
 app.use(express.json())
 
 app.use(routes)
 
-database()
-
-app.use(( error, request, response, next) => {
-  if(error instanceof AppError) {
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
     return response.status(error.statusCode).json({
       status: "error",
       message: error.message
@@ -30,7 +28,6 @@ app.use(( error, request, response, next) => {
   })
 
 })
-
 
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
