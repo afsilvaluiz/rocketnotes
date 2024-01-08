7096578880097afd1b019efe041e6f8b5d635ab3
luiz <afsilvaluiz@gmail.com>
2024-01-08 13:05:03 -0300

    test email in use' method for creating new users has been implemented.

--- src/services/UserCreateService.spec.js
+++ src/services/UserCreateService.spec.js
@@ -1,17 +1,41 @@
 const UserCreateService = require('./UserCreateService')
 const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory")
+const AppError = require("../utils/AppError")
 
-it("user should be created", async () => {
-  const user = {
-    name: "User Test",
-    email: "user@test.com",
-    password: "123"
-  }
 
-  const userRepositoryInMemory = new UserRepositoryInMemory()
-  const userCreateService = new UserCreateService(userRepositoryInMemory)
-  const userCreated = await userCreateService.execute(user)
+describe("UserCreateService", () => {
+  it("user should be created", async () => {
+    const user = {
+      name: "User Test",
+      email: "user@test.com",
+      password: "123"
+    }
+  
+    const userRepositoryInMemory = new UserRepositoryInMemory()
+    const userCreateService = new UserCreateService(userRepositoryInMemory)
+    const userCreated = await userCreateService.execute(user)  
+  
+    expect(userCreated).toHaveProperty("id")  
+  })
 
-  expect(userCreated).toHaveProperty("id")
+  it("users cannot be created with an existing email", async () => {
+    const user1 = {
+      name: "User Test 1",
+      email: "user@test.com",
+      password: "123"
+    }
+
+    const user2 = {
+      name: "User Test 2",
+      email: "user@test.com",
+      password: "456"
+    }
+
+    const userRepository = new UserRepositoryInMemory()
+    const userCreateService = new UserCreateService(userRepository)
+
+    await userCreateService.execute(user1)
+    await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError("This email is already in use."))
+  })
 
 })
